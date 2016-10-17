2adc128a40c6cd25b279d81f418fa0f75b9af485
William Morris <william.morris@net-a-porter.com>
2016-10-17 17:27:46 +0100

    Started section 4

--- ReduxSimpleStarter-YouTube/src/index.js
+++ ReduxSimpleStarter-YouTube/src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyCiIb4jN2wl6ILAyT1zm3b-QWZVzZ8q3yw';
+const API_KEY = '';
 
 //Create a new component. This component should produce some HTML
 class App extends Component { //'() =>' is a similar way of writing 'function()'
